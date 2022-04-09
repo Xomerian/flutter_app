54b90a6db798a84e98acb1d89a8718073775fa68
Xomerian <77843593+Xomerian@users.noreply.github.com>
2022-04-09 12:45:01 +0300

    Add logo
    Click on logo to go back to login

--- lib/main.dart
+++ lib/main.dart
@@ -6,22 +6,11 @@ void main() {
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
-
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter project',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
           primarySwatch: Colors.amber,
           accentColor: Colors.amber,
           brightness: Brightness.light,
@@ -45,89 +34,26 @@ class MyApp extends StatelessWidget {
 
 class MainPage extends StatefulWidget {
   const MainPage({Key? key}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
   final String title = 'Flutter Project';
-
   @override
   State<MainPage> createState() => _MainState();
 }
 
 class _MainState extends State<MainPage> {
-  int _counter = 0;
   int _selectedPage = 0;
-  void _onItemTapped(int index) {
-    setState(() {
-      _selectedPage = index;
-    });
-    if (index==0) _navigateToAlertScreen(context);
-      else if(index==1) _navigateToCalculatorScreen(context);
-      else _navigateToShopScreen(context);
-  }
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
+
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
+        leading: IconButton(
+          onPressed: () {_navigateToLoginScreen(context);},
+          icon : Image.asset('img/logo_flutter.png'),
         ),
-
-      ),bottomNavigationBar: BottomNavigationBar(
+      ),
+      bottomNavigationBar: BottomNavigationBar(
             items: const <BottomNavigationBarItem>[
             BottomNavigationBarItem(
             icon: Icon(Icons.warning),
@@ -145,14 +71,22 @@ class _MainState extends State<MainPage> {
             currentIndex: _selectedPage,
             selectedItemColor: Colors.amber[800],
             onTap: _onItemTapped,
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
       ), // This trailing comma makes auto-formatting nicer for build methods.
 
     );
+  }
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedPage = index;
+    });
+    if (index==0) _navigateToAlertScreen(context);
+    else if(index==1) _navigateToCalculatorScreen(context);
+    else _navigateToShopScreen(context);
+  }
+  void _navigateToLoginScreen(BuildContext context) {
+    Navigator.of(context)
+        .push(MaterialPageRoute(builder: (context) => LoginPage()));
   }
   void _navigateToAlertScreen(BuildContext context) {
     Navigator.of(context)
@@ -166,6 +100,7 @@ class _MainState extends State<MainPage> {
     Navigator.of(context)
         .push(MaterialPageRoute(builder: (context) => ShopPage()));
   }
+
 }
 
 
